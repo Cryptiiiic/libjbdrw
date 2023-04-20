73fa4bb1bbd515ad11a96141ee9e8478f6146a15
Cryptiiiic <liamwqs@gmail.com>
2023-04-20 09:40:57 -0700

    Add kcall initializers

--- src/jbdrw.m
+++ src/jbdrw.m
@@ -12,24 +12,45 @@ static int kbasehelper(uint64_t *kbase) {
     return 0;
 }
 
+static bool jbd_is_initialized = false;
+
 __attribute__((used))
 krw_plugin_initializer_t krw_initializer(krw_handlers_t handlers) {
     handlers->version = (uint64_t)(VERSION);
-    int ret = jbdInitPPLRW();
-    if(ret) {
-        JBLogDebug("[!]: %s: %s: Failed jbdInitPPLRW! (Version: %llu)\n", __FILE__, __func__, handlers->version);
-        return (krw_plugin_initializer_t)-1;
+    if(!jbd_is_initialized) {
+      int ret = jbdInitPPLRW();
+      if(ret) {
+          JBLogDebug("[!]: %s: %s: Failed jbdInitPPLRW! (Version: %llu)\n", __FILE__, __func__, handlers->version);
+          return (krw_plugin_initializer_t)-1;
+      }
+      jbd_is_initialized = true;
+      JBLogDebug("[*]: %s: %s: Successfully initialized jbdInitPPLRW! (Version: %llu)\n", __FILE__, __func__, handlers->version);
     }
-    JBLogDebug("[*]: %s: %s: Successfully initialized jbdInitPPLRW! (Version: %llu)\n", __FILE__, __func__, handlers->version);
 
     handlers->kbase = (krw_kbase_func_t)(kbasehelper);
     handlers->kread = (krw_kread_func_t)(kreadbuf);
     handlers->kwrite = (krw_kwrite_func_t)(kwritebuf);
     handlers->kmalloc = (krw_kmalloc_func_t)(kalloc);
     handlers->kdealloc = (krw_kdealloc_func_t)(kfree);
-    handlers->kcall = (krw_kcall_func_t)(jbdKcall);
-    handlers->physread = (krw_physread_func_t)(physreadbuf);
-    handlers->physwrite = (krw_physwrite_func_t)(physwritebuf);
     JBLogDebug("[*]: %s: %s: Successfully initialized jbdrw krw plugin! (Version: %llu)\n", __FILE__, __func__, handlers->version);
     return 0;
 }
+
+__attribute__((used))
+krw_plugin_initializer_t kcall_initializer(krw_handlers_t handlers) {
+  if(!jbd_is_initialized) {
+    int ret = jbdInitPPLRW();
+    if(ret) {
+        JBLogDebug("[!]: %s: %s: Failed jbdInitPPLRW! (Version: %llu)\n", __FILE__, __func__, handlers->version);
+        return (krw_plugin_initializer_t)-1;
+    }
+    jbd_is_initialized = true;
+    JBLogDebug("[*]: %s: %s: Successfully initialized jbdInitPPLRW! (Version: %llu)\n", __FILE__, __func__, handlers->version);
+  }
+  handlers->kcall = (krw_kcall_func_t)(jbdKcall);
+  handlers->physread = (krw_physread_func_t)(physreadbuf);
+  handlers->physwrite = (krw_physwrite_func_t)(physwritebuf);
+  JBLogDebug("[*]: %s: %s: Successfully initialized jbdrw krw plugin! (Version: %llu)\n", __FILE__, __func__, handlers->version);
+  return 0;
+}
+
